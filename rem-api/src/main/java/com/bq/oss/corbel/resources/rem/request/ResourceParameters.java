c90e48f662b73b2ff6551ddf063992dc6b96c4b7
Francisco Sanchez <francisco.sanchez@bq.com>
2015-05-27 14:45:08 +0200

    Refactor resources to support api:conditions in PUT resources && Resmi findAndModify Support
    Change-Id: I21110c383137a341f7161a5d62762637daa3cb78

--- rem-api/src/main/java/com/bq/oss/corbel/resources/rem/request/ResourceParameters.java
+++ rem-api/src/main/java/com/bq/oss/corbel/resources/rem/request/ResourceParameters.java
@@ -1,6 +1,20 @@
 package com.bq.oss.corbel.resources.rem.request;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.bq.oss.lib.queries.request.ResourceQuery;
+
 /**
  * @author Alexander De Leon
  */
-public interface ResourceParameters {}
+public interface ResourceParameters {
+
+
+    default Optional<List<ResourceQuery>> getConditions() {
+        return Optional.empty();
+    }
+
+    default void setConditions(Optional<List<ResourceQuery>> resources) {}
+
+}
---
+++ rem-api/src/main/java/com/bq/oss/corbel/resources/rem/request/ResourceParametersImpl.java
@@ -0,0 +1,26 @@
+package com.bq.oss.corbel.resources.rem.request;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.bq.oss.lib.queries.jaxrs.QueryParameters;
+import com.bq.oss.lib.queries.parser.AggregationParser;
+import com.bq.oss.lib.queries.parser.QueryParser;
+import com.bq.oss.lib.queries.parser.SortParser;
+
+/**
+ * Created by Francisco Sanchez on 26/05/15.
+ */
+public class ResourceParametersImpl extends QueryParameters implements ResourceParameters {
+
+    public ResourceParametersImpl(int pageSize, int page, int maxPageSize, Optional<String> sort, Optional<List<String>> query,
+            QueryParser queryParser, Optional<String> aggregation, AggregationParser aggregationParser, SortParser sortParser,
+            Optional<String> search) {
+        super(pageSize, page, maxPageSize, sort, query, queryParser, aggregation, aggregationParser, sortParser, search);
+    }
+
+    public ResourceParametersImpl(QueryParameters queryParameters) {
+        super(queryParameters);
+    }
+
+}
