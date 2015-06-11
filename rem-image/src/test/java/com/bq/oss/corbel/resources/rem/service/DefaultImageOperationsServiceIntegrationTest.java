561e1fb52d2494872e3fcc644115e24d36618cd9
vipereybq <victor.perezrey@bq.com>
2015-06-11 16:46:13 +0200

    [Rebase][Patch 11] SILKROAD-1530. Change imageType in rem-image
    Change-Id: I051fc97247bb20b1d72013b4911a525f688af7b5

--- rem-image/src/test/java/com/bq/oss/corbel/resources/rem/service/DefaultImageOperationsServiceIntegrationTest.java
+++ rem-image/src/test/java/com/bq/oss/corbel/resources/rem/service/DefaultImageOperationsServiceIntegrationTest.java
@@ -1,5 +1,15 @@
 package com.bq.oss.corbel.resources.rem.service;
 
+import com.bq.oss.corbel.resources.rem.exception.ImageOperationsException;
+import com.bq.oss.corbel.resources.rem.format.ImageFormat;
+import com.bq.oss.corbel.resources.rem.model.ImageOperationDescription;
+import com.bq.oss.corbel.resources.rem.operation.*;
+import com.google.common.collect.ImmutableMap;
+import org.im4java.core.IM4JavaException;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,25 +17,17 @@ import java.net.URL;
 import java.util.Collections;
 import java.util.List;
 
-import org.im4java.core.IM4JavaException;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-
-import com.bq.oss.corbel.resources.rem.exception.ImageOperationsException;
-import com.bq.oss.corbel.resources.rem.model.ImageOperationDescription;
-import com.bq.oss.corbel.resources.rem.operation.*;
-import com.google.common.collect.ImmutableMap;
-
 @Ignore// Needs imagemagick package installed
 public class DefaultImageOperationsServiceIntegrationTest {
 
+    private static final String IMAGE_PATH = "src/test/resources/logo.png";
     private static final String IMAGE_URL = "http://upload.wikimedia.org/wikipedia/commons/b/b6/SIPI_Jelly_Beans_4.1.07.tiff";
+    private ImageFormat imageFormatNull;
 
     private DefaultImageOperationsService imageOperationsService;
 
     @Before
-    public void setUp() {
+    public void setUp() throws ImageOperationsException {
         imageOperationsService = new DefaultImageOperationsService(new DefaultImageOperationsService.IMOperationFactory(),
                 new DefaultImageOperationsService.ConvertCmdFactory(), ImmutableMap.<String, ImageOperation>builder()
                         .put("resizeWidth", new ResizeWidth()).put("resizeHeight", new ResizeHeight()).put("resize", new Resize())
@@ -37,8 +39,7 @@ public class DefaultImageOperationsServiceIntegrationTest {
     public void test() throws IOException, ImageOperationsException, InterruptedException, IM4JavaException {
         try (InputStream image = new URL(IMAGE_URL).openStream(); FileOutputStream out = new FileOutputStream("/tmp/testImage.tiff")) {
             List<ImageOperationDescription> parameters = Collections.singletonList(new ImageOperationDescription("crop", "(0, 0, 2, 2)"));
-            imageOperationsService.applyConversion(parameters, image, out);
+            imageOperationsService.applyConversion(parameters, image, out, new ImageFormat());
         }
     }
-
 }
