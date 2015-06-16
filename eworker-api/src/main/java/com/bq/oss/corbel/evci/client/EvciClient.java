e3a39561048fc26fda37166cc7f7de9639a33b15
Rubén Carrasco <rcarrasco@bq.com>
2015-06-16 14:51:56 +0200

    SILKROAD-1581 Move evci to github
    Change-Id: Ica57e300e6ddbae19f935ca963daeca0ccb564be

---
+++ eworker-api/src/main/java/com/bq/oss/corbel/evci/client/EvciClient.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2014 StarTIC
+ */
+package com.bq.oss.corbel.evci.client;
+
+/**
+ * @author Alexander De Leon
+ *
+ */
+public interface EvciClient {
+
+	/**
+	 * Internally send a EVCI event
+	 * 
+	 * @param type
+	 *            the type of the event (e.g. music:Play)
+	 * @param event
+	 *            The java object to be serialized into the JSON event data
+	 */
+	void convertAndSend(String type, Object event);
+
+}
