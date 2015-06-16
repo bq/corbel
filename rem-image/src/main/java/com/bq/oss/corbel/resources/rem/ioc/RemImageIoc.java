ff18efbf0d62517de73a34732484937ee6d3e71a
Luis Miguel Garcia <luismiguel.garcia@bq.com>
2015-06-16 16:39:12 +0200

    Add rem-image caches invalidation.
    Change-Id: I89b7aee27859eeda8e8e45aff20acbfa99ab2d98

--- rem-image/src/main/java/com/bq/oss/corbel/resources/rem/ioc/RemImageIoc.java
+++ rem-image/src/main/java/com/bq/oss/corbel/resources/rem/ioc/RemImageIoc.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import com.bq.oss.corbel.resources.rem.ImageDeleteRem;
+import com.bq.oss.corbel.resources.rem.ImagePutRem;
+import com.bq.oss.corbel.resources.rem.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,15 +17,12 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import com.bq.oss.corbel.resources.rem.ImageGetRem;
 import com.bq.oss.corbel.resources.rem.Rem;
 import com.bq.oss.corbel.resources.rem.operation.*;
-import com.bq.oss.corbel.resources.rem.service.DefaultImageCacheService;
-import com.bq.oss.corbel.resources.rem.service.DefaultImageOperationsService;
-import com.bq.oss.corbel.resources.rem.service.ImageCacheService;
-import com.bq.oss.corbel.resources.rem.service.ImageOperationsService;
 import com.bq.oss.lib.config.ConfigurationIoC;
 
 @SuppressWarnings("unused") @Configuration @EnableAsync @Import({ConfigurationIoC.class}) public class RemImageIoc {
 
     @Autowired private Environment env;
+    @Autowired private RemService remService;
 
     @Bean
     public static Map<String, ImageOperation> getOperations(List<ImageOperation> imageOperationList) {
@@ -73,6 +73,16 @@ import com.bq.oss.lib.config.ConfigurationIoC;
     @Bean(name = RemImageIocNames.REM_GET)
     public Rem getImageGetRem(ImageOperationsService imageOperationsService, ImageCacheService imageCacheService) {
         return new ImageGetRem(imageOperationsService, imageCacheService);
+    }
+
+    @Bean(name = RemImageIocNames.REM_PUT)
+    public Rem getImagePutRem(RemService remService) {
+        return new ImagePutRem(remService, env.getProperty("image.cache.collection", "image:ImageCache"));
+    }
+
+    @Bean(name = RemImageIocNames.REM_DELETE)
+    public Rem getImageDeleteRem(RemService remService) {
+        return new ImageDeleteRem(remService, env.getProperty("image.cache.collection", "image:ImageCache"));
     }
 
 }
