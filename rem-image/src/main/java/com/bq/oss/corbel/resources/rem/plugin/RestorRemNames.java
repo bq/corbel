201a1e4454f86eac2bf0593993d2606eddd7a890
Ismael Madirolas <ismael.madirolas@bq.com>
2015-07-17 10:58:57 +0200

    fix problem with rem-acl and rem-image in put and delete too
    Change-Id: I01e048b62a4e45a298c8360a3047e2d097bfd53d

---
+++ rem-image/src/main/java/com/bq/oss/corbel/resources/rem/plugin/RestorRemNames.java
@@ -0,0 +1,8 @@
+package com.bq.oss.corbel.resources.rem.plugin;
+
+public interface RestorRemNames {
+
+    String RESTOR_GET = "RestorGet";
+    String RESTOR_PUT = "RestorPut";
+    String RESTOR_DELETE = "RestorDelete";
+}
