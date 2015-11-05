3876b32e0b7f927864653efe044372e19a0d9b16
Luis Miguel Garcia <luismiguel.garcia@bq.com>
2015-11-05 10:34:44 +0100

    Add admins feature to rem-acl.
    Change-Id: If5086c24db583600ba2b3450dcbb6e2a55d3b23e

--- rem-acl/src/main/java/io/corbel/resources/rem/acl/AclGetRem.java
+++ rem-acl/src/main/java/io/corbel/resources/rem/acl/AclGetRem.java
@@ -1,21 +1,7 @@
 package io.corbel.resources.rem.acl;
 
-import io.corbel.lib.queries.request.ResourceQuery;
-import io.corbel.lib.ws.api.error.ErrorResponseFactory;
-import io.corbel.resources.rem.Rem;
-import io.corbel.resources.rem.acl.query.AclQueryBuilder;
-import io.corbel.resources.rem.request.CollectionParameters;
-import io.corbel.resources.rem.request.CollectionParametersImpl;
-import io.corbel.resources.rem.request.RelationParameters;
-import io.corbel.resources.rem.request.RequestParameters;
-import io.corbel.resources.rem.request.ResourceId;
-import io.corbel.resources.rem.request.ResourceParameters;
-import io.corbel.resources.rem.service.AclResourcesService;
-import io.corbel.resources.rem.utils.AclUtils;
-
 import java.io.InputStream;
 import java.net.URI;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -24,6 +10,17 @@ import javax.ws.rs.core.Response;
 
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+
+import com.google.gson.JsonObject;
+
+import io.corbel.lib.queries.request.ResourceQuery;
+import io.corbel.lib.token.TokenInfo;
+import io.corbel.lib.ws.api.error.ErrorResponseFactory;
+import io.corbel.resources.rem.Rem;
+import io.corbel.resources.rem.acl.query.AclQueryBuilder;
+import io.corbel.resources.rem.request.*;
+import io.corbel.resources.rem.service.AclResourcesService;
+import io.corbel.resources.rem.utils.AclUtils;
 
 /**
  * @author Cristian del Cerro
@@ -38,10 +35,11 @@ public class AclGetRem extends AclBaseRem {
     @Override
     public Response resource(String type, ResourceId id, RequestParameters<ResourceParameters> parameters, Optional<InputStream> entity) {
 
-        String userId = parameters.getTokenInfo().getUserId();
-        Collection<String> groupIds = parameters.getTokenInfo().getGroups();
+        TokenInfo tokenInfo = parameters.getTokenInfo();
 
-        return aclResourcesService.getResourceIfIsAuthorized(userId, groupIds, type, id, AclPermission.READ).map(originalObject -> {
+        Optional<JsonObject> resource = aclResourcesService.getResourceIfIsAuthorized(tokenInfo, type, id, AclPermission.READ);
+
+        return resource.map(originalObject -> {
             if (parameters.getAcceptedMediaTypes().contains(MediaType.APPLICATION_JSON)) {
                 return Response.ok(originalObject).build();
             }
@@ -54,13 +52,15 @@ public class AclGetRem extends AclBaseRem {
 
     @Override
     public Response collection(String type, RequestParameters<CollectionParameters> parameters, URI uri, Optional<InputStream> entity) {
-        String userId = parameters.getTokenInfo().getUserId();
-        Collection<String> groupIds = parameters.getTokenInfo().getGroups();
         if (!parameters.getAcceptedMediaTypes().contains(MediaType.APPLICATION_JSON)) {
             return ErrorResponseFactory.getInstance().methodNotAllowed();
         }
 
-        addAclQueryParams(parameters, userId, groupIds);
+        TokenInfo tokenInfo = parameters.getTokenInfo();
+
+        if (!aclResourcesService.isManagedBy(tokenInfo, type)) {
+            addAclQueryParams(parameters, tokenInfo);
+        }
 
         Rem resmiRem = remService.getRem(type, JSON_MEDIATYPE, HttpMethod.GET, Collections.singletonList(this));
         Response response = aclResourcesService.getCollection(resmiRem, type, parameters);
@@ -72,30 +72,12 @@ public class AclGetRem extends AclBaseRem {
         return Response.ok(response.getEntity()).build();
     }
 
-    @Override
-    public Response relation(String type, ResourceId id, String relation, RequestParameters<RelationParameters> parameters,
-            Optional<InputStream> entity) {
+    private void addAclQueryParams(RequestParameters<CollectionParameters> parameters, TokenInfo tokenInfo) {
 
-        String userId = parameters.getTokenInfo().getUserId();
-        if (id.isWildcard()) {
-            return ErrorResponseFactory.getInstance().methodNotAllowed();
-        }
-
-        Collection<String> groupIds = parameters.getTokenInfo().getGroups();
-
-        if (aclResourcesService.isAuthorized(userId, groupIds, type, id, AclPermission.READ)) {
-            Rem rem = remService.getRem(type, parameters.getAcceptedMediaTypes(), HttpMethod.GET, Collections.singletonList(this));
-            return aclResourcesService.getRelation(rem, type, id, relation, parameters);
-        }
-        return ErrorResponseFactory.getInstance().unauthorized(AclUtils.buildMessage(AclPermission.READ));
-
-    }
-
-    private void addAclQueryParams(RequestParameters<CollectionParameters> parameters, String userId, Collection<String> groupIds) {
         Optional<CollectionParameters> collectionParameters = parameters.getOptionalApiParameters();
 
-        List<ResourceQuery> aclQueryParams = new AclQueryBuilder(userId, groupIds).build(collectionParameters.flatMap(
-                CollectionParameters::getQueries).orElse(Collections.emptyList()));
+        List<ResourceQuery> aclQueryParams = new AclQueryBuilder(Optional.ofNullable(tokenInfo.getUserId()), tokenInfo.getGroups())
+                .build(collectionParameters.flatMap(CollectionParameters::getQueries).orElse(Collections.emptyList()));
 
         if (collectionParameters.isPresent()) {
             collectionParameters.get().setQueries(Optional.of(aclQueryParams));
@@ -104,6 +86,25 @@ public class AclGetRem extends AclBaseRem {
                     Optional.of(aclQueryParams), Optional.empty(), Optional.empty(), Optional.empty());
             collectionParameters = Optional.of(collectionParametersImpl);
         }
+    }
+
+    @Override
+    public Response relation(String type, ResourceId id, String relation, RequestParameters<RelationParameters> parameters,
+            Optional<InputStream> entity) {
+
+        if (id.isWildcard()) {
+            return ErrorResponseFactory.getInstance().methodNotAllowed();
+        }
+
+        TokenInfo tokenInfo = parameters.getTokenInfo();
+
+        if (aclResourcesService.isAuthorized(tokenInfo, type, id, AclPermission.READ)) {
+            Rem rem = remService.getRem(type, parameters.getAcceptedMediaTypes(), HttpMethod.GET, Collections.singletonList(this));
+            return aclResourcesService.getRelation(rem, type, id, relation, parameters);
+        }
+
+        return ErrorResponseFactory.getInstance().unauthorized(AclUtils.buildMessage(AclPermission.READ));
+
     }
 
 }
